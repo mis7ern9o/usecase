82dbf2f49b8a83918f567ed9a80667ba766b9d97
mis7er.n9o <9377756+mis7ern9o@users.noreply.github.com>
2022-10-03 18:54:50 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,58 +1,45 @@
 import json
 import os
-import csv
 import pandas as pd
+import time
 
-curdir=os.path.dirname(__file__)
-inMap=os.path.join(curdir,'mapping.json')
-inData=os.path.join(curdir,'dataset.csv')
-outData=os.path.join(curdir,'dataset_out.csv')
+# generic and simple mapping script
+# https://www.onlinedatagenerator.com/
+
+# TODO Add  formulas to destination file
+# TODO Add more rows to dataset.csv
+
+tic = time.perf_counter()
+curdir = os.path.dirname(__file__)
+inMap = os.path.join(curdir, 'mapping.json')
+inData = os.path.join(curdir, 'dataset200k.csv')
+outData = os.path.join(curdir, 'dataset_out.csv')
 
 # parse json into dic
 fmap = open(inMap)
 dmap = json.load(fmap)
 
-#open dataset into dataframe
-fdata = pd.read_csv(inData,delimiter=",", encoding="utf8")
+# open dataset into dataframe
+fdata = pd.read_csv(inData, delimiter=",", encoding="utf8")
 idx_dic = {}
 for col in fdata.columns:
     idx_dic[fdata.columns.get_loc(col)] = col
 
 fdatanum = len(fdata)
 
+# map dataset
 frame = {}
 for col in dmap:
     label = dmap[col]['label']
     colSource = dmap[col]['source']
-    print(label, col, colSource)
-
     if colSource == "none":
         default = dmap[col]['default']
-        print('check>>',dmap[col]['fx'])
         frame[label] = pd.Series(default, index=range(fdatanum))
     else:        
         frame[label] = pd.Series(fdata.iloc[:, int(colSource)])
 
-#frame = { 'Author': auth_series, 'Article': article_series }
-#Creating DataFrame by passing Dictionary
 result = pd.DataFrame(frame)
-print(result.columns)
-    
-
-
-
-
-#print(fdata.iloc[:, 3])
-
-
-
-
-
-
-
-
-
-
-#
-
+result.to_csv(outData, sep=';', index=False)
+toc = time.perf_counter()
+print(f"Speed {toc - tic:0.4f} seconds")
 
